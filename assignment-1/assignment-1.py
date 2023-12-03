b5361b25db3ae040f9be34f1ffe1284c55d98489
UJJWAL KUMAR <2204ujjwalkumar@gmail.com>
2023-12-03 12:17:04 +0530

    ps-1 completed

--- assignment-1/assignment-1.py
+++ assignment-1/assignment-1.py
@@ -1,30 +1,23 @@
 '''
 Group Members
-    Ankush Niroula (CS23BTKMU11002)
-    Anish Manandhar (CS23BTKMU11001)
-    Shristy Sharma (EE23RESCH11007)
-    Aakriti Bhandari (EE23BTKMU11001)
-    Saurav Bista (EE23BTKMU11002)
+    Ujjwal Kumar (CS23BTNSK11002)
+    Anvitha (CS23BTNSK11001)
     
-Note:
-Please name the input file as 'input1.csv' and place it in the same folder as this python code.
-Then run the file to get sequence of vertices visited and the value of the objective function at that vertex.
-
 '''
 
-from ctypes import _NamedFuncPointer
 import numpy as np
 import csv
+import pandas as pd
 
-threshold_value = pow(10, -8)
+threshold_val_Th = pow(10, -8)
 
-class SimplexAlgorithm:
-    def _init_(self, A, b, c, z):
-        self.A = A
-        self.b = b
-        self.c = c
+class Simpex_Algorithm:
+    def __init__(self, w, x, y, z):
+        self.w = w
+        self.x = x
+        self.y = y
         self.z = z
-        size = A.shape
+        size = w.shape
         self.m = size[0]
         self.n = size[1]
 
@@ -32,17 +25,17 @@ class SimplexAlgorithm:
 
         result = self.main_simplex()
         print(f"Optimal solution is {result}")
-        print(f"The Maximum Objective Value is {np.dot(self.c, result)}")
+        print(f"The Maximum Objective Value is {np.dot(self.y, result)}")
 
 
         print("\nSequence of Visited Vertices:")
         for vertex in self.visited_vertices:
-            print(f"Vertex: {vertex}, Objective Value: {np.dot(self.c, vertex)}")
+            print(f"Vertex: {vertex}, Objective Value: {np.dot(self.y, vertex)}")
 
     def calculate_direction(self):
-        x = np.dot(self.A, self.z) - self.b
-        e_indices = np.where(np.abs(x) < threshold_value)[0]
-        self.A_dash = self.A[e_indices]
+        x = np.dot(self.w, self.z) - self.x
+        e_indices = np.where(np.abs(x) < threshold_val_Th)[0]
+        self.A_dash = self.w[e_indices]
         self.direction_vector = -np.linalg.inv(np.transpose(self.A_dash))
 
     def find_maximum_alpha(self):
@@ -55,26 +48,26 @@ class SimplexAlgorithm:
 
     def find_feasible_neighbour(self):
         self.calculate_direction()
-        cost_vector = np.dot(self.direction_vector, self.c)
+        cost_vector = np.dot(self.direction_vector, self.y)
         positive_cost_indices = np.where(cost_vector > 0)[0]
 
         if len(positive_cost_indices) == 0:
             return None
         else:
             self.u = self.direction_vector[positive_cost_indices[0]]
-            x = np.abs(np.dot(self.A, self.z) - self.b)
-            e_ind = np.where(x < threshold_value)[0]
-            non_e_ind = ~np.isin(np.arange(len(self.A)), e_ind)
+            x = np.abs(np.dot(self.w, self.z) - self.x)
+            e_ind = np.where(x < threshold_val_Th)[0]
+            non_e_ind = ~np.isin(np.arange(len(self.w)), e_ind)
 
-            self.n_A = self.A[non_e_ind]
-            self.n_b = self.b[non_e_ind]
+            self.n_A = self.w[non_e_ind]
+            self.n_b = self.x[non_e_ind]
 
             self.find_maximum_alpha()
 
             return self.z + self.alpha * self.u
 
     def main_simplex(self):
-        if np.all(np.dot(self.A, self.z) - self.b <= threshold_value):
+        if np.all(np.dot(self.w, self.z) - self.x <= threshold_val_Th):
             self.visited_vertices.append(self.z.copy())
             
         while True:
@@ -86,20 +79,17 @@ class SimplexAlgorithm:
                 self.visited_vertices.append(result)
         return self.z
 
-def main():
-    # Read input from CSV file
-    with open('input1.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        data = list(reader)
 
+data=pd.read_csv("assignment-1\input1.csv",header=None)
+data=data.fillna('')
+data = data.astype(str)
+data= data.values.tolist()
+print(data)
+print("\n")
     # Extract data from CSV
-    z = np.array([float(val) for val in data[0][:-1]])
-    c = np.array([float(val) for val in data[1][:-1]])
-    b = np.array([float(row[-1]) for row in data[2:]])
-    A = np.array([[float(val) for val in row[:-1]] for row in data[2:]])
-
+z = np.array([float(value) for value in data[0][:-1]])
+y = np.array([float(value) for value in data[1][:-1]])
+x = np.array([float(row[-1]) for row in data[2:]])
+w = np.array([[float(value) for value in row[:-1]] for row in data[2:]])
     # Call the constructor and run the functions
-    SimplexAlgorithm(A, b, c, z)
-
-if _NamedFuncPointer == '_main_':
-    main()
+Simpex_Algorithm(w, x, y, z)
